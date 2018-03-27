6fa56240bdbd3920043b74e1c39397bd69c5c120
Teun <teun.willems+github@live.nl>
2018-03-27 10:22:45 +0200

    Added previous field to LocationFacade

--- src/main/kotlin/com/pts62/common/facade/LocationFacade.kt
+++ src/main/kotlin/com/pts62/common/facade/LocationFacade.kt
@@ -6,5 +6,6 @@ data class LocationFacade(
         val vehicle: VehicleFacade,
         val latitude: Double,
         val longtitude: Double,
-        val timestamp: String
+        val timestamp: String,
+        val previousTimestamp: String
 ) : Serializable
