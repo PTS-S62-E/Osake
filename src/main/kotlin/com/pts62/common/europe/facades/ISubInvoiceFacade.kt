2157aa316c380ea5b2e42c23db09c1d844b7e605
teunw <teun.willems+github@live.nl>
2018-05-07 10:15:18 +0200

    Fixed issue #1

--- src/main/kotlin/com/pts62/common/europe/facades/ISubInvoiceFacade.kt
+++ src/main/kotlin/com/pts62/common/europe/facades/ISubInvoiceFacade.kt
@@ -1,17 +1,19 @@
 package com.pts62.common.europe.facades
 
 import com.rekeningrijden.europe.interfaces.ISubInvoice
+import java.time.LocalDateTime
 
 open class ISubInvoiceFacade(
         private val invoiceNumber: String,
         private val country: String,
         private val paymentStatus: Boolean,
-        private val invoiceDate: String,
+        private val invoiceDate: LocalDateTime,
         private val price: Int
 ) : ISubInvoice {
     override fun getInvoiceNumber() = this.invoiceNumber
     override fun getPrice() = (this.price.toDouble() / 100.0)
     override fun getPaymentStatus() = this.paymentStatus
     override fun getCountry() = this.country
-    override fun getInvoiceDate() = this.invoiceDate
+    override fun getInvoiceDate() = this.invoiceDate.toString()
+    fun getInvoiceAsLocalDateTime() = this.invoiceDate
 }
