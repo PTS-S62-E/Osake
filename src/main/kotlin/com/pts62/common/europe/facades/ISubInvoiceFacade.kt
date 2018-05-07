377bf6d1c6c919e9d6e15f110559ebdaa69db1d3
Teun Willems <mj.willems@student.fontys.nl>
2018-05-07 10:34:16 +0200

    Dev localdate (#11)
    * Commit for syncing
    
    * Finished communication
    
    * Fixed issue #1
    
    * LocalDateTime compilation fix

--- src/main/kotlin/com/pts62/common/europe/facades/ISubInvoiceFacade.kt
+++ src/main/kotlin/com/pts62/common/europe/facades/ISubInvoiceFacade.kt
@@ -7,13 +7,13 @@ open class ISubInvoiceFacade(
         private val invoiceNumber: String,
         private val country: String,
         private val paymentStatus: Boolean,
-        private val invoiceDate: LocalDateTime,
+        private val invoiceDate: String,
         private val price: Int
 ) : ISubInvoice {
     override fun getInvoiceNumber() = this.invoiceNumber
     override fun getPrice() = (this.price.toDouble() / 100.0)
     override fun getPaymentStatus() = this.paymentStatus
     override fun getCountry() = this.country
-    override fun getInvoiceDate() = this.invoiceDate.toString()
-    fun getInvoiceAsLocalDateTime() = this.invoiceDate
+    override fun getInvoiceDate() = this.invoiceDate
+    fun getInvoiceAsLocalDateTime() = LocalDateTime.parse(this.invoiceDate)
 }
