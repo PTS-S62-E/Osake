1c58eb4012b2be37bb5f3d8c3915b920d661dd91
teunw <teun.willems+github@live.nl>
2018-05-22 09:45:37 +0200

    Fixed classes for rest responses

--- src/main/kotlin/com/pts62/common/facade/administration/OwnershipFacade.kt
+++ src/main/kotlin/com/pts62/common/facade/administration/OwnershipFacade.kt
@@ -5,7 +5,7 @@ import java.sql.Timestamp
 
 open class OwnershipFacade(
         val owner: OwnerFacade,
-        val fromDate: String,
-        val toDate: String,
+        val readbleFromDate: String,
+        val readableToDate: String?,
         val vehicleId: Int
 ) : Serializable
