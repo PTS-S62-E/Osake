2310fa0cca1665c3804b170b64dcc78381dab7f5
Teun Willems <mj.willems@student.fontys.nl>
2018-05-29 09:25:41 +0200

    Beginning antaminenservice (#13)

--- src/main/kotlin/com/pts62/common/finland/communication/rest/AntaminenConfiguration.kt
+++
@@ -1,9 +0,0 @@
-package com.pts62.common.finland.communication.rest
-
-class AntaminenConfiguration : IServiceConfiguration {
-    override fun getServiceAddress() = ""
-
-    override fun getServiceCredential() = ""
-
-    override fun getServicePassword() = ""
-}
